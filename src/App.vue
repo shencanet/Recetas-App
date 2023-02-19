317e64936795bb5e43ac119dfe9969da19c9485d
shen <santi.dionis@gmail.com>
2023-02-19 21:34:48 +0100

    ADD: Buscador

--- src/App.vue
+++ src/App.vue
@@ -2,50 +2,72 @@
 
 import HelloWorld from './components/HelloWorld.vue'
 import axios from 'axios';
+import Meal from './components/Meal.vue'
 import Category from './components/Category.vue'
 </script>
 
 <template>
-<HelloWorld msg="Api Restauracion || Santi Dionis" />
+  <HelloWorld msg="Api Restauracion || Santi Dionis" />
+  <h1>Buscador Recetas</h1>
+  <input type="text" v-model="search" v-on:keyup.enter="searchData" placeholder="Buscador Aqui ">
+  <Meal 
+  v-for="meal in meals" 
+  v-bind:key="meal.idMeal" 
+  v-bind:meal="meal" />
 
-
-<Category 
-v-for="category in categories"  
-v-bind:key="category.idCategory" 
-v-bind:category="category" 
-/>
-
-
-
-
+  <Category v-for="category in categories" v-bind:key="category.idCategory" v-bind:category="category" />
 </template>
 
 <script>
 export default {
   name: 'App',
-  components:{
+  components: {
     Category,
+    
   },
-  data(){
+  data() {
     return {
-      categories: []
+      categories: [],
+      meals: [],
+      search: null
 
     };
-  }, mounted(){
+  }, mounted() {
     axios.get('https://www.themealdb.com/api/json/v1/1/categories.php')
-    .then((res) => {
-      console.log(res.data.categories);
-      
+      .then((res) => {
+        console.log(res.data.categories);
 
 
 
-      this.categories = res.data.categories;
+
+        this.categories = res.data.categories;
 
 
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  },
+  methods: {
+    searchData() {
+      if (this.search) {
+        axios.get('https://www.themealdb.com/api/json/v1/1/search.php?s=' + this.search)
+          .then((res) => {
+            this.meals = res.data.meals;
+
+            //idMeal strCategory  strMealThumb
+
+
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+      } else {
+        alert('Ingresa un texto')
+      }
+
+
+    }
   }
 }
 </script>
@@ -86,14 +108,15 @@ nav a {
 nav a:first-of-type {
   border: 0;
 }
-.category_container{
-    border: 1px solid green;
-    padding: 50px;
-    border-radius: 50px;
-   margin-top: 25px;
-  
 
-  }
+.category_container {
+  border: 1px solid green;
+  padding: 50px;
+  border-radius: 50px;
+  margin-top: 25px;
+
+
+}
 
 @media (min-width: 1024px) {
   header {
@@ -120,6 +143,6 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
- 
+
 }
 </style>
---
+++ src/components/Meal.vue
@@ -0,0 +1,16 @@
+<template>
+
+    <div class="category_container">
+        <img v-bind:src="meal.strMealThumb">
+        <h6>{{ meal.strMeal }}</h6>
+        <p><b>Categoria</b>: {{ meal.strCategory }} | Origen: {{ meal.strArea }} </p>
+
+    </div>
+</template>
+<script>
+
+export default {
+    props: ["meal"]
+}
+
+</script>
